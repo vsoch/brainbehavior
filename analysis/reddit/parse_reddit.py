cdba492919cfad51e636aea01512645d4f950c11
vsoch <vsochat@stanford.edu>
2015-08-15 01:08:59 -0700

    modified:   analysis/reddit/0.get_reddit.py 	modified:   analysis/reddit/parse_reddit.py

--- analysis/reddit/parse_reddit.py
+++ analysis/reddit/parse_reddit.py
@@ -15,22 +15,26 @@ content = []
 ids = []
 scores = []
 for sub in submissions:
+    try:
         if len(sub.selftext) > 0:
-            content.append(sub.selftext)
-            ids.append(sub.fullname)
-            scores.append(sub.score)
-            if sub.num_comments > 0:
-                comments = sub.comments
-                while len(comments) > 0:
-                    for comment in comments:
-                        current = comments.pop(0)
-                        if isinstance(current,praw.objects.MoreComments):
-                            comments = comments + current.comments()
-                        else:               
-                            if len(current.body)>0:     
-                                content.append(current.body)
-                                ids.append(current.fullname)
-                                scores.append(current.score)
+                if sub.fullname not in ids:
+                    content.append(sub.selftext)
+                    ids.append(sub.fullname)
+                    scores.append(sub.score)
+                    if sub.num_comments > 0:
+                        comments = sub.comments
+                        while len(comments) > 0:
+                            for comment in comments:
+                                current = comments.pop(0)
+                                if isinstance(current,praw.objects.MoreComments):
+                                    comments = comments + current.comments()
+                                else:               
+                                    if len(current.body)>0:     
+                                        content.append(current.body)
+                                        ids.append(current.fullname)
+                                        scores.append(current.score)
+    except:
+        print "Skipping %s" %sub.fullname
 
 print "%s has %s entities" %(disorder,len(content))
 result = {"content":content,"disorder":disorder,"score":scores,"uids":ids,"retrieved":now}
