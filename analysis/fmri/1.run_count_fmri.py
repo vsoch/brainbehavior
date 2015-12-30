d2ac93ff5d85306066314d70246cbcf1909a67d2
vsoch <vsochat@stanford.edu>
2015-12-30 13:50:11 -0800

    updating submission script to dynamically submit jobs if room in the queue, as sherlock limit was reduced to 1000. Really annoying.

--- analysis/fmri/1.run_count_fmri.py
+++ analysis/fmri/1.run_count_fmri.py
@@ -9,17 +9,22 @@ outfolder = "/share/PI/russpold/work/PUBMED/fmri/counts"
 
 # This pickle has a list of our terms
 term_pickle =  "/home/vsochat/SCRIPT/brainbehavior/brainbehavior/data/fmri.pkl"
+folders = subfolders[:]
+count = 0
 
-for s in range(0,len(subfolders)):
-    subfolder = subfolders[s]
-    if not os.path.exists("%s/%s_counts.pkl" %(outfolder,subfolder)):
-        jobfile = open(".job/%s.job" %subfolder,'w')
-        jobfile.writelines("#!/bin/bash\n")
-        jobfile.writelines("#SBATCH --job-name=%s_count.job\n" %(subfolder))
-        jobfile.writelines("#SBATCH --output=.out/%s_count.out\n" %(subfolder))
-        jobfile.writelines("#SBATCH --error=.out/%s_count.err\n" %(subfolder)) 
-        jobfile.writelines("#SBATCH --time=2-00:00\n") 
-        jobfile.writelines("#SBATCH --mem=12000\n")   
-        jobfile.writelines("python /home/vsochat/SCRIPT/brainbehavior/analysis/fmri/1.count_fmri.py %s %s %s %s\n" %(topfolder,subfolder,term_pickle,outfolder))  
-        jobfile.close()
-        os.system('sbatch -p russpold .job/%s.job' %subfolder)
+while len(folders)>0:
+    subfolder = folders[0]
+    queue_count = int(os.popen("squeue -u vsochat | wc -l").read().strip("\n"))
+    if queue_count < 1000:
+        if not os.path.exists("%s/%s_counts.pkl" %(outfolder,subfolder)):
+            jobfile = open(".job/%s.job" %subfolder,'w')
+            jobfile.writelines("#!/bin/bash\n")
+            jobfile.writelines("#SBATCH --job-name=%s_count.job\n" %(subfolder))
+            jobfile.writelines("#SBATCH --output=.out/%s_count.out\n" %(subfolder))
+            jobfile.writelines("#SBATCH --error=.out/%s_count.err\n" %(subfolder)) 
+            jobfile.writelines("#SBATCH --time=2-00:00\n") 
+            jobfile.writelines("#SBATCH --mem=12000\n")   
+            jobfile.writelines("python /home/vsochat/SCRIPT/brainbehavior/analysis/fmri/1.count_fmri.py %s %s %s %s\n" %(topfolder,subfolder,term_pickle,outfolder))  
+            jobfile.close()
+            os.system('sbatch -p russpold .job/%s.job' %subfolder)
+        folders.pop(0) 
